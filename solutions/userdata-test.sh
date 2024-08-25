5d32f19cf4ee90a163fd4f01ccfb4e652e9019eb
Seyma <seymanurseckinn@gmail.com>
2024-08-25 15:31:23 +0200

    tekrar

--- solutions/userdata-test.sh
+++ solutions/userdata-test.sh
@@ -6,9 +6,9 @@ pip3 install Flask==2.3.3
 pip3 install Flask-MySql
 pip3 install boto3
 dnf install git -y
-echo "seymanur-phonebook-app.cbanmzptkrzf.us-east-1.rds.amazonaws.com" > /home/ec2-user/dbserver.endpoint
+echo "seymanur-phonebook-18.c9gc8sa2ih1i.us-east-1.rds.amazonaws.com" > /home/ec2-user/dbserver.endpoint
 cd /home/ec2-user
 TOKEN=$(aws --region=us-east-1 ssm get-parameter --name /clarusway/phonebook/token --with-decryption --query 'Parameter.Value' --output text)
-git clone https://$TOKEN@github.com/Seymanurkara/phonebook-web-app.git
-python3 /home/ec2-user/phonebook-web-app/solutions/phonebook-app.py 
+git clone https://$TOKEN@github.com/Seymanurkara/Phonebook-Project
+python3 /home/ec2-user/Phonebook-Project/solutions/phonebook-app.py 
 
